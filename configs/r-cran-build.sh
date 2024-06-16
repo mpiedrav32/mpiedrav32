b2180d76021494d2e2af61110dc8803af7ff3105
Marco Piedra Venegas <mpiedrav@proton.me>
2024-06-16 03:48:08 -0600

    config

--- configs/r-cran-build.sh
+++ configs/r-cran-build.sh
@@ -1,24 +1,15 @@
-# https://docs.posit.co/resources/install-r-source.html
+# Based on https://docs.posit.co/resources/install-r-source.html
 
-export R_VERSION=4.4.1
+export R_VERSION="4.4.1"
 
 wget https://cran.r-project.org/src/base/R-4/R-${R_VERSION}.tar.gz
 tar -xzvf R-${R_VERSION}.tar.gz
 cd R-${R_VERSION}
 
-./configure \
-  --prefix=/opt/R/${R_VERSION} \
-  --enable-R-shlib \
-  --enable-memory-profiling \
-  --with-blas \
-  --with-lapack
+./configure --prefix="$HOME/R-${R_VERSION}" --enable-R-shlib --enable-memory-profiling
 
 make
-sudo make install
+make install
 
-/opt/R/${R_VERSION}/bin/R --version
-
-sudo ln -s /opt/R/${R_VERSION}/bin/R /usr/local/bin/R
-sudo ln -s /opt/R/${R_VERSION}/bin/Rscript /usr/local/bin/Rscript
-
-echo "export RSTUDIO_WHICH_R=/opt/R/${R_VERSION}/bin/R" >> $HOME/.profile
+echo "export RSTUDIO_WHICH_R=$HOME/R-${R_VERSION}/bin/R" >> $HOME/.profile
+echo "export PATH=$HOME/R-${R_VERSION}/bin" >> $HOME/.profile
